b6c03b09070099ad636ea83c529d911503603be8
Ammar Jussa <ammar26497@gmail.com>
2020-11-04 18:08:07 +0500

    Added firebase and sass

--- whatsapp_client/src/App.js
+++ whatsapp_client/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Working Nicely</h1>
+
+      {/* Components: 
+        1) Sidebar
+
+        2) Chat Component
+      */}
     </div>
   );
 }
---
+++ whatsapp_client/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAMjgbUT492_qkjlRfVtC0tI7AcyABjfeA",
+    authDomain: "whatsapp-clone-caa6a.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-caa6a.firebaseio.com",
+    projectId: "whatsapp-clone-caa6a",
+    storageBucket: "whatsapp-clone-caa6a.appspot.com",
+    messagingSenderId: "40563666002",
+    appId: "1:40563666002:web:861c4fad0098d9c668438b",
+    measurementId: "G-Y1S8MWSRVG"
+  };
